012f0b8dddb668b7cced911037509c9fee276ec6
MICHAEL JOHN BROOKE <201377881@student.uj.ac.za>
2018-05-14 18:25:05 +0200

    Initial commit

---
+++ src/main/java/brooke/michael/twitterfeed/TwitterfeedApplication.java
@@ -0,0 +1,12 @@
+package brooke.michael.twitterfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterfeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterfeedApplication.class, args);
+    }
+}
