ade9cfa377df3fb281f275fe7624fadedba12ebe
Michael Brooke <michaelbrooke93@gmail.com>
2018-05-18 00:01:04 +0200

    Fixed up the Tweet and User Service Unit Tests after the Service/Builder split.

--- src/test/java/brooke/michael/twitterfeed/service/impl/TweetServiceTest.java
+++ src/test/java/brooke/michael/twitterfeed/service/impl/TweetServiceTest.java
@@ -1,42 +1,61 @@
 package brooke.michael.twitterfeed.service.impl;
 
+import brooke.michael.twitterfeed.map.UserMap;
+import brooke.michael.twitterfeed.map.impl.UserTreeMap;
+import brooke.michael.twitterfeed.model.User;
+import brooke.michael.twitterfeed.model.builder.TweetBuilder;
+import brooke.michael.twitterfeed.reader.impl.TwitterTextFileReader;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
 
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
 public class TweetServiceTest {
 
+    @Mock
+    private TwitterTextFileReader twitterTextFileReader;
 
-//    @Test
-//    public void addTweets_success() {
-//        //Given we have a map of the following 2 users
-//        UserMap userMap = new UserTreeMap();
-//        userMap.put(new User("Alan"));
-//        userMap.put(new User("Ward"));
-//
-//        //And we have the following set of tweets relating to those users
-//        when(twitterTextFileReader.readFile(anyString())).thenReturn(Arrays.asList(
-//                "Alan> If you have a procedure with 10 parameters, you probably missed some.",
-//                "Ward> There are only two hard things in Computer Science: cache invalidation, naming things and off-by-1 errors.",
-//                "Alan> Random numbers should not be generated with a method chosen at random."
-//        ));
-//
-//        //When we add tweets
-//        tweetBuilder.addTweets(userMap);
-//
-//        //Then the tweets are appropriately assigned to the Users in the UserMap, with the correct owner and content
-//        assertEquals(2, userMap.get("Alan").getTweets().size());
-//        assertEquals("If you have a procedure with 10 parameters, you probably missed some.", userMap.get("Alan").getTweets().get(0).getContent());
-//        assertEquals("Alan", userMap.get("Alan").getTweets().get(0).getOwner());
-//        assertEquals("Random numbers should not be generated with a method chosen at random.", userMap.get("Alan").getTweets().get(1).getContent());
-//        assertEquals("Alan", userMap.get("Alan").getTweets().get(1).getOwner());
-//
-//        assertEquals(1, userMap.get("Ward").getTweets().size());
-//        assertEquals("There are only two hard things in Computer Science: cache invalidation, naming things and off-by-1 errors.", userMap.get("Ward").getTweets().get(0).getContent());
-//        assertEquals("Ward", userMap.get("Ward").getTweets().get(0).getOwner());
-//    }
+    @Spy
+    private TweetBuilder tweetBuilder;
+
+    @InjectMocks
+    private TweetService tweetService;
+
+    @Before
+    public void before() {
+        ReflectionTestUtils.setField(tweetService, "tweetsFilePath", "C:/Users/some_user/tweets.txt");
+    }
 
     @Test
-    public void getTweets() {
+    public void addTweets_success() {
+        //Given we have a map of the following 2 users
+        UserMap userMap = new UserTreeMap();
+        userMap.put(new User("Alan"));
+        userMap.put(new User("Ward"));
+
+        //And we have the following set of tweets relating to those users
+        when(twitterTextFileReader.readFile(anyString())).thenReturn(Arrays.asList(
+                "Alan> If you have a procedure with 10 parameters, you probably missed some.",
+                "Ward> There are only two hard things in Computer Science: cache invalidation, naming things and off-by-1 errors.",
+                "Alan> Random numbers should not be generated with a method chosen at random."
+        ));
+
+        //When we add tweets
+        tweetService.getTweets(userMap);
+
+        //Then the tweets are appropriately assigned to the Users in the UserMap, with the correct owner and content
+        assertEquals(2, userMap.get("Alan").getTweets().size());
+        assertEquals(1, userMap.get("Ward").getTweets().size());
     }
 }
--- src/test/java/brooke/michael/twitterfeed/service/impl/UserServiceTest.java
+++ src/test/java/brooke/michael/twitterfeed/service/impl/UserServiceTest.java
@@ -1,34 +1,52 @@
 package brooke.michael.twitterfeed.service.impl;
 
+import brooke.michael.twitterfeed.map.UserMap;
+import brooke.michael.twitterfeed.model.builder.UserBuilder;
+import brooke.michael.twitterfeed.reader.impl.TwitterTextFileReader;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
 public class UserServiceTest {
 
-//    @Before
-//    public void before() {
-//        ReflectionTestUtils.setField(userBuilder, "usersFilePath", "C:/Users/some_user/users.txt");
-//    }
-//
-//    @Test
-//    public void buildUsers_success() {
-//        //Given the following valid lines representing users and the users they follow
-//        when(twitterTextFileReader.readFile(anyString())).thenReturn(Arrays.asList(
-//                "Ward follows Alan",
-//                "Alan follows Martin",
-//                "Ward follows Martin, Alan"
-//        ));
-//
-//        //When users are built
-//        UserMap userMap = userBuilder.buildUsers();
-//
-//        assertEquals(3, userMap.size());
-//        assertEquals(1, userMap.get("Alan").getFollowing().size());
-//        assertEquals(2, userMap.get("Ward").getFollowing().size());
-//    }
+    @Mock
+    private TwitterTextFileReader twitterTextFileReader;
+
+    @Spy
+    private UserBuilder userBuilder;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Before
+    public void before() {
+        ReflectionTestUtils.setField(userService, "usersFilePath", "C:/Users/some_user/users.txt");
+    }
 
     @Test
-    public void getUsers() {
+    public void buildUsers_success() {
+        //Given the following valid lines representing users and the users they follow
+        when(twitterTextFileReader.readFile(anyString())).thenReturn(Arrays.asList(
+                "Ward follows Alan",
+                "Alan follows Martin",
+                "Ward follows Martin, Alan"
+        ));
+
+        //When users are built
+        UserMap userMap = userService.getUsers();
+
+        assertEquals(3, userMap.size());
     }
 }
